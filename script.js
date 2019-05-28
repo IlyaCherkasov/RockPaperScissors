f6eb3a9e607e9fe7317482c07a437917b4b2ee3d
Cherkasik <ilyakopeysk@gmail.com>
2019-05-28 18:08:07 +0300

    yay, it is working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -26,18 +26,43 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)]
 }
 
+function convert(choice){
+    switch(choice){
+        case 'r':
+            return "Камень";
+        case 's':
+            return "Ножницы";
+        case 'p':
+            return "Бумага";
+    }
+}
+
+function win(user, comp){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = convert(user) + " - ваш выбор, " + convert(comp) + " - выбор компьютера. Вы победили!!! Поздравляю!";
+    
+}
+
+function lose(user, comp){
+    compScore++;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = convert(user) + " - ваш выбор, " + convert(comp) + " - выбор компьютера. Вы проиграли...";
+}
+
 function game(userChoice){
-    const ComputerChoice = getComputerChoice();
-    switch (userChoice + ComputerChoice){
-        case "rs", "pr", "sp":
-            console.log("Вы победили.");
+    const compChoice = getComputerChoice();
+    console.log(userChoice, compChoice);
+    switch (userChoice + compChoice){
+        case 'rs': case 'pr': case 'sp':
+            win(userChoice, compChoice);
             break;
-        case "rp", "ps", "sr":
-            console.log("Вы проиграли");
-            break
-        case "rr", "pp", "ss":
-            console.log("ничья");
-            break
+        case 'rp': case 'ps': case 'sr':
+            lose(userChoice, compChoice);
+            break;
+        case "rr": case "pp": case "ss":
+            result_div.innerHTML = convert(userChoice) + " - это то, что выбрали все. Ничья!";
+            break;
     }
 }
 
