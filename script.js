a7ff42d0ac0044eaf3a676d173bbacf2732f25c2
Ilya Cherkasov <ilyakopeysk@gmail.com>
2020-09-05 13:23:04 +0300

    doing better some kind of design

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let userScore = 0;
 let compScore = 0;
-const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("comp-score");
-const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result > p");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const userScore_span = document.getElementById('user-score');
+const compScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result > p');
+const explain_p = document.getElementById('action-message');
+const emote_p = document.getElementById('emote');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
 function main(){
     rock_div.addEventListener('click', function(){
         game('r');
@@ -29,18 +31,20 @@ function getComputerChoice(){
 function convert(choice){
     switch(choice){
         case 'r':
-            return "Камень";
+            return 'Камень';
         case 's':
-            return "Ножницы";
+            return 'Ножницы';
         case 'p':
-            return "Бумага";
+            return 'Бумага';
     }
 }
 
 function win(user, comp){
     userScore++;
     userScore_span.innerHTML = userScore;
-    result_div.innerHTML = convert(user) + " - ваш выбор, " + convert(comp) + " - выбор компьютера. Вы победили!!! Поздравляю!";
+    result_div.innerHTML = 'Вы победили';
+    explain_p.innerHTML = convert(user) + ' - ваш выбор, ' + convert(comp) + ' - выбор компьютера.';
+    emote_p.innerHTML = 'ヽ(o^▽^o)ﾉ';
     document.getElementById(user).classList.add('green-glow');
     setTimeout(function(){document.getElementById(user).classList.remove('green-glow')}, 300);
 }
@@ -48,13 +52,17 @@ function win(user, comp){
 function lose(user, comp){
     compScore++;
     compScore_span.innerHTML = compScore;
-    result_div.innerHTML = convert(user) + " - ваш выбор, " + convert(comp) + " - выбор компьютера. Вы проиграли...";
+    result_div.innerHTML = 'Вы проиграли';
+    explain_p.innerHTML = convert(user) + ' - ваш выбор, ' + convert(comp) + ' - выбор компьютера.';
+    emote_p.innerHTML = '(っ˘̩╭╮˘̩)っ';
     document.getElementById(user).classList.add('red-glow');
     setTimeout(function(){document.getElementById(user).classList.remove('red-glow')}, 300);
 }
 
 function draw(user, comp){
-    result_div.innerHTML = convert(user) + " - это то, что выбрали все. Ничья!";
+    result_div.innerHTML = 'Ничья';
+    explain_p.innerHTML = convert(user) + ' - это то, что выбрали все.';
+    emote_p.innerHTML = '┐(￣～￣)┌';
     document.getElementById(user).classList.add('yellow-glow');
     setTimeout(function(){document.getElementById(user).classList.remove('yellow-glow')}, 300);
 }
